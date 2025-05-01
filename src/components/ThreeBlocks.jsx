a2d2e884a0de172501aca6d0b275f70787d76e16
cjethan <56584033+cjethan@users.noreply.github.com>
2025-05-01 11:33:46 +0200

    Three blocks updates
    Additional Information first, no more days/ weeks toggle

--- src/components/ThreeBlocks.jsx
+++ src/components/ThreeBlocks.jsx
@@ -61,6 +61,30 @@ const ThreeBlocks = ({ randomIntent, setHowOften, setHowImp }) => {
       </div>
       {!isCollapsed && (
         <div className="grid grid-cols-3 gap-4">
+          {/* Third Block */}
+          <div className="p-4 bg-gray-100 rounded shadow relative">
+            <h3 className="font-bold mb-2">Additional Information about the Intent</h3>
+            <div className="absolute top-2 right-2 group">
+              <div className="w-6 h-6 flex items-center justify-center bg-gray-200 text-gray-700 rounded-full cursor-pointer">
+                i
+              </div>
+              <div className="absolute top-8 right-0 hidden group-hover:block bg-white text-gray-700 text-sm p-4 rounded shadow-lg w-64 z-10">
+                Additional ideas about how to categorize songs with this intent.
+              </div>
+            </div>
+            <p className="italic text-gray-700">
+              {randomIntent ? randomIntent.main_listening_function : 'No intent available'}
+            </p>
+            <p className="text-gray-700">
+              {randomIntent?.listening_functions.slice(0, 3).map((functionName, index) => (
+                functionName && functionName !== randomIntent.main_listening_function ? (
+                  <React.Fragment key={index}>
+                    {functionName}<br />
+                  </React.Fragment>
+                ) : null
+              ))}
+            </p>
+          </div>
           {/* First Block */}
           <div className="p-4 bg-gray-100 rounded shadow relative">
             <h3 className="font-bold mb-2">How often do you listen with this intent?</h3>
@@ -69,25 +93,11 @@ const ThreeBlocks = ({ randomIntent, setHowOften, setHowImp }) => {
                 i
               </div>
               <div className="absolute top-8 right-0 hidden group-hover:block bg-white text-gray-700 text-sm p-4 rounded shadow-lg w-64 z-10">
-                Aproximate how much you listen to music with this intent. <br />
-                Choose days/weeks or songs/minutes acording to how it is easier to answer for you.
+                Approximate how much you listen to music with this intent. <br />
+                Choose weeks or songs/minutes according to how it is easier to answer for you.
               </div>
             </div>
-            {/* Stylish, smaller switches */}
             <div className="flex items-center mb-4 space-x-6">
-              <div className="flex items-center">
-                <span className="text-gray-600 text-sm mr-2">Days/Weeks</span>
-                <label className="relative inline-flex items-center cursor-pointer">
-                  <input
-                    type="checkbox"
-                    className="sr-only peer"
-                    checked={usePlaceholderLabels1}
-                    onChange={() => setUsePlaceholderLabels1(!usePlaceholderLabels1)}
-                  />
-                  <div className="w-8 h-4 bg-gray-300 rounded-full peer peer-checked:bg-blue-200 transition-colors"></div>
-                  <span className="absolute left-0.5 top-0.5 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4"></span>
-                </label>
-              </div>
               <div className="flex items-center">
                 <span className="text-gray-600 text-sm mr-2">Songs/Minutes</span>
                 <label className="relative inline-flex items-center cursor-pointer">
@@ -102,50 +112,13 @@ const ThreeBlocks = ({ randomIntent, setHowOften, setHowImp }) => {
                 </label>
               </div>
             </div>
-            {/* Determine labels based on switches */}
-            {[
-              usePlaceholderLabels1 && usePlaceholderLabels2
-                ? "<63 min/week"
-                : usePlaceholderLabels1
-                ? "<21 songs/week"
-                : usePlaceholderLabels2
-                ? "<9 min/day"
-                : "<3 songs/day",
-              usePlaceholderLabels1 && usePlaceholderLabels2
-                ? "63 min/week"
-                : usePlaceholderLabels1
-                ? "21 songs/week"
-                : usePlaceholderLabels2
-                ? "9 min/day"
-                : "3 songs/day",
-              usePlaceholderLabels1 && usePlaceholderLabels2
-                ? "189 min/week"
-                : usePlaceholderLabels1
-                ? "49 songs/week"
-                : usePlaceholderLabels2
-                ? "27 min/day"
-                : "9 songs/day",
-              usePlaceholderLabels1 && usePlaceholderLabels2
-                ? "378 min/week"
-                : usePlaceholderLabels1
-                ? "126 songs/week"
-                : usePlaceholderLabels2
-                ? "54 min/day"
-                : "18 songs/day",
-              usePlaceholderLabels1 && usePlaceholderLabels2
-                ? "504 min/week"
-                : usePlaceholderLabels1
-                ? "168 songs/week"
-                : usePlaceholderLabels2
-                ? "72 min/day"
-                : "24 songs/day",
-              usePlaceholderLabels1 && usePlaceholderLabels2
-                ? ">504 min/week"
-                : usePlaceholderLabels1
-                ? ">168 songs/week"
-                : usePlaceholderLabels2
-                ? ">72 min/day"
-                : ">24 songs/day",
+            { [
+              usePlaceholderLabels2 ? "<9 min/week" : "<3 songs/week",
+              usePlaceholderLabels2 ? "9 min/week" : "3 songs/week",
+              usePlaceholderLabels2 ? "27 min/week" : "9 songs/week",
+              usePlaceholderLabels2 ? "54 min/week" : "18 songs/week",
+              usePlaceholderLabels2 ? "72 min/week" : "24 songs/week",
+              usePlaceholderLabels2 ? ">72 min/week" : ">24 songs/week",
             ].map((option, index) => (
               <label key={option} className="flex items-center space-x-3 text-gray-600">
                 <input
@@ -176,7 +149,7 @@ const ThreeBlocks = ({ randomIntent, setHowOften, setHowImp }) => {
               "Not important / Never listening with this intent",
               "Little important",
               "Medium important",
-              "Realatively important",
+              "Relatively important",
               "Very important",
             ].map((option, index) => (
               <label key={option} className="flex items-center space-x-3 text-gray-600">
@@ -191,31 +164,6 @@ const ThreeBlocks = ({ randomIntent, setHowOften, setHowImp }) => {
                 <span>{option}</span>
               </label>
             ))}
-          </div>
-
-          {/* Third Block */}
-          <div className="p-4 bg-gray-100 rounded shadow relative">
-            <h3 className="font-bold mb-2">Additional Information</h3>
-            <div className="absolute top-2 right-2 group">
-              <div className="w-6 h-6 flex items-center justify-center bg-gray-200 text-gray-700 rounded-full cursor-pointer">
-                i
-              </div>
-              <div className="absolute top-8 right-0 hidden group-hover:block bg-white text-gray-700 text-sm p-4 rounded shadow-lg w-64 z-10">
-                Additional ideas about how to categorize songs with this intent.
-              </div>
-            </div>
-            <p className="italic text-gray-700">
-              {randomIntent ? randomIntent.main_listening_function : 'No intent available'}
-            </p>
-            <p className="text-gray-700">
-              {randomIntent?.listening_functions.slice(0, 3).map((functionName, index) => (
-                functionName && functionName !== randomIntent.main_listening_function ? (
-                  <React.Fragment key={index}>
-                    {functionName}<br />
-                  </React.Fragment>
-                ) : null
-              ))}
-            </p>
           </div>
         </div>
       )}
